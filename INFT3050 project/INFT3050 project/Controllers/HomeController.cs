dfdd92f0638c9100cb0a5d9c755da1bf9a980fe2
Blake Eastood <blakeeastwood2000@gmail.com>
2023-07-27 09:36:36 +1000

    creation of project
    initial commit

---
+++ INFT3050 project/INFT3050 project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using INFT3050_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace INFT3050_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ INFT3050 project/INFT3050 project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INFT3050_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
