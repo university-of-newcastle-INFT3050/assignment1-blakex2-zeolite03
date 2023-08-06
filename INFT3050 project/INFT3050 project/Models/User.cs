c94bee0ba95435fbc29c33ab011e0f118bb854bb
BlakeEveleigh <Blake.w.r.eveleigh@gmail.com>
2023-08-06 15:36:49 +1000

    Created Users

---
+++ INFT3050 project/INFT3050 project/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+//created by Eveleigh 6/08/2023
+namespace INFT3050_project.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Key]
+        public string UserName { get; set; }
+        
+        public string Email { get; set; }
+
+        // ask Connor how to possibly add the salt and hash to this user and other models
+
+        //the sql data will populate but we will still need to make a hash function and slat function for the patron model
+        public string HashedPW { get; set; }
+        public string Name { get; set; }
+        public bool IsAdmin { get; set; }
+                
+        public int Salt { get; set; }
+
+    }
+}
