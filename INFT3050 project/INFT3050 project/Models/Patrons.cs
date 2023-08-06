7a2b0a26e51f5b6ec73c2461d134550f24046520
BlakeEveleigh <Blake.w.r.eveleigh@gmail.com>
2023-08-06 15:53:25 +1000

    Updated patron and users

--- INFT3050 project/INFT3050 project/Models/Patrons.cs
+++ INFT3050 project/INFT3050 project/Models/Patrons.cs
@@ -1,6 +1,30 @@
-﻿namespace INFT3050_project.Models
+﻿using System.ComponentModel.DataAnnotations;
+//created by Eveleigh 6/08/2023 WIP
+namespace INFT3050_project.Models
 {
     public class Patrons
     {
-    }
+        [Key]
+        public int UserId { get; set; }
+
+        //email is used as a username
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a Email.")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a  name.")]
+        [MaxLength(255)]
+
+        //so the hash function and salt function will need to be created for when a new patron creates an account
+
+        //32 hexadecimal digits generated at random
+        public string Salt { get; set; }
+        [MaxLength(32)]
+
+        //SHA256 hash value of salt + password (password appended to salt value)
+
+        public string HashedPW { get; set; }
+        [Required(ErrorMessage = "Please enter a password.")]
+        [MaxLength(64)]
+    } 
 }
--- INFT3050 project/INFT3050 project/Models/User.cs
+++ INFT3050 project/INFT3050 project/Models/User.cs
@@ -1,5 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
-//created by Eveleigh 6/08/2023
+//created by Eveleigh 6/08/2023 WIP
 namespace INFT3050_project.Models
 {
     public class User
@@ -8,17 +8,29 @@ namespace INFT3050_project.Models
 
         [Key]
         public string UserName { get; set; }
-        
+        [Required(ErrorMessage = "Please enter a User name.")]
+        [MaxLength(50)]
+
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a Email.")]
+        [MaxLength(255)]
 
         // ask Connor how to possibly add the salt and hash to this user and other models
 
         //the sql data will populate but we will still need to make a hash function and slat function for the patron model
+
+        //SHA256 hash value of salt + password (password appended to salt value)
         public string HashedPW { get; set; }
+        [Required(ErrorMessage = "Please enter a password.")]
+        [MaxLength(64)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a name.")]
+        [MaxLength(255)]
         public bool IsAdmin { get; set; }
                 
-        public int Salt { get; set; }
+        //32 hexadecimal digits generated at random
+        public string Salt { get; set; }
+        [MaxLength(32)]
 
     }
 }
