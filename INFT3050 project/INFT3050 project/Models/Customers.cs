82eadc29758eb802f891271205786026e8b7c9f0
BlakeEveleigh <Blake.w.r.eveleigh@gmail.com>
2023-08-06 15:09:36 +1000

    added customers

---
+++ INFT3050 project/INFT3050 project/Models/Customers.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_1.Models
+{
+    public class Customers
+    {
+        
+
+        
+        //just making get and set for data that will happen whena customer account is created
+        public int CustomersId { get; set; }
+
+        public string FirstName { get; set; } 
+        [Required(ErrorMessage = "Please enter a first name.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please enter a Last name.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please enter a Password.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a Email.")]
+
+    }
+}
